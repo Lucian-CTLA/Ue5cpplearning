29b02fb609d522d8721ed0953bedab96abc10d45
Lucian-CTLA <LucianCantarella@outlook.com>
2026-08-25 17:09:20 +0800

    Initial commit: C++ learning projects (Toublic1, ifStatement)

---
+++ Toublic1/Toublic1/Source.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char my_character;
+	my_character = 'y';
+
+	int my_int;
+	my_int = 10;
+
+	cout << my_character << endl;
+	cout << my_int << endl;
+
+	my_character = 'n';
+	my_int = 20;
+	cout << my_character << endl;
+	cout << my_int << endl;
+
+	
+
+	system("pause");
+
+}
