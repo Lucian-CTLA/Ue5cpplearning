9693bae80d5ec7e35a50b7b671adcb7f401b4806
Lucian-CTLA <LucianCantarella@outlook.com>
2026-08-31 11:13:22 +0800

    Add Array project

---
+++ Array/Array/Array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int MyArray[5] = { 1,23,5,4,9 };
+	for(int i=0;i<5;i++)
+	{ 
+		cout << "MyArray[" <<i<<"] : " << MyArray[i] << endl;
+	
+	}
+	
+
+	system("pause");
+}
