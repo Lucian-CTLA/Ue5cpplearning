9ab8961e82b875f9aaccc9014112e170cd9cfc23
BieYufei <LucianCantarella@outlook.com>
2026-09-06 19:51:32 +0800

    Add casting and 多态 projects

---
+++ 多态/多态/多态.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+class Object
+{
+public:
+	virtual void BeginPlay();
+};
+
+class Actor : public Object
+{
+public:
+	virtual void BeginPlay() override;
+};
+
+class Pawn : public Actor
+{
+public:
+	virtual void BeginPlay() override;
+};
+
+int main()
+{
+	Object* ptr_to_object = new Object;
+	Actor* ptr_to_Actor = new Actor;
+	Pawn* ptr_to_pawn = new Pawn;
+
+	Object* ObjectArray[] = {ptr_to_object,ptr_to_Actor,ptr_to_pawn};
+
+	for (int i = 0;i < 3;i++)
+	{
+		ObjectArray[i]->BeginPlay();
+	}
+
+	delete ptr_to_object;
+	delete ptr_to_Actor;
+	delete ptr_to_pawn;
+	system("pause");
+}
+
+void Object::BeginPlay()
+{
+	cout << "Object BeginPly() called" << endl;
+}
+
+void Actor::BeginPlay()
+{
+	cout << "Actor BeginPlay() called" << endl;
+}
+
+void Pawn::BeginPlay()
+{
+	cout << "Pawn BeginPlay() called" << endl;
+}
